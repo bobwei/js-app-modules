906d169aa3cbf7e1ce7188c4cce850b8fb0382c9
bobwei <bob61016@gmail.com>
2017-07-31 18:10:53 +0800

    init with js-app-modules

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-// @flow
-import 'dotenv/config';
-
-console.log('Hello World');
+export {
+  default as createValidation,
+} from './forms/validations/createValidation';
+export {
+  default as createPathValidation,
+} from './forms/validations/createPathValidation';
+export {
+  default as createRequiredValidations,
+} from './forms/validations/createRequiredValidations';
+export {
+  default as createMatchValidation,
+} from './forms/validations/createMatchValidation';
